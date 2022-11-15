c99b8c48625e468e8e4815ddc63516da7ab2ac12
Luiz Pereira <lfhbpereira@gmail.com>
2022-11-15 19:44:50 -0300

    Update requirement 3

--- src/routers/loginRouter.js
+++ src/routers/loginRouter.js
@@ -3,7 +3,7 @@ const generateToken = require('../helpers/generateToken');
 
 const router = express.Router();
 
-router.post('/', async (_req, res) => {
+router.post('/', (_req, res) => {
   res.status(200).json({ token: generateToken() });
 });
 
